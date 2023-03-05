f3bf9263771fd4581374f635f1a7ac811642c56c
Nick Santana <nick@mobilecoin.com>
2023-03-05 12:04:34 -0800

    Initial template of main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io::{self, Read, Write, BufReader}, process::exit};
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    if env::args().any(|x| x == "--version") {
+        println!(env!("CARGO_PKG_VERSION"));
+        exit(0);
+    }
+
+    let stdin = BufReader::new(io::stdin().lock());
+    let out = run_plugin(stdin)?;
+
+    io::stdout().write_all(&out)?;
+    Ok(())
+}
+
+fn run_plugin<R:Read>(_reader: BufReader<R>) -> Result<Vec<u8>> {
+    Ok(b"what it is?".to_vec())
 }
